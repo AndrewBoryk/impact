417112de4d4f654f2a5929162a7baf8a50e49580
Andrew Boryk <andrewcboryk@gmail.com>
2015-03-02 22:25:02 -0500

    Initial Commit

---
+++ menu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  menu
+//
+//  Created by Andrew Boryk on 3/2/15.
+//  Copyright (c) 2015 Andrew Boryk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ menu/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  menu
+//
+//  Created by Andrew Boryk on 3/2/15.
+//  Copyright (c) 2015 Andrew Boryk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
